cc767b777dee9e0112915d834d84dfa3e1426747
vinay thallam <vinay.thallam@jifflenow.com>
2018-07-08 21:14:33 +0530

    redux-promise in action

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,8 @@
 export default function(state = null, action){
+
+	//Here payload contains the actial data from the api response instead of the promise object even if the action creator is returning promise object
+	//This is due to redux-promise middleware. It checks if the payload has promise object, and constructs new action to be passed to reducers which contains resolved data
+	console.log("action.payload :: ", action.payload)
+
 	return state
 }
