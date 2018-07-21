96ec2cdfc5f61d230978e9625d2b68ab13a2d77e
vinay thallam <vinay.thallam@jifflenow.com>
2018-07-21 23:53:28 +0530

    weather list component

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
 import SearchBar from '../containers/search_bar'
+import WeatherList from '../containers/weather_list'
 
 export default class App extends Component {
   render() {
@@ -15,6 +16,7 @@ export default class App extends Component {
       	<div className="row">
       		weather forcast app
       		<SearchBar/>
+          <WeatherList/>
       	</div>
       </div>
     );
