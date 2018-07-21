143235dabb163fdff13d28e58cc62a751fc2f141
vinay thallam <vinay.thallam@jifflenow.com>
2018-07-21 23:39:25 +0530

    update state with weather data

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,15 @@
-export default function(state = null, action){
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action){
 
 	//Here payload contains the actial data from the api response instead of the promise object even if the action creator is returning promise object
 	//This is due to redux-promise middleware. It checks if the payload has promise object, and constructs new action to be passed to reducers which contains resolved data
 	console.log("action.payload :: ", action.payload)
-
+	switch(action.type) {
+		case FETCH_WEATHER :
+			//state should never be manipulated/mutated. i.e you should not do state.push(action.payload.data) here.
+			//Always return a new state. concat here returns a new array. it does not modify the existing array
+			return [action.payload.data].concat(state)
+	}
 	return state
 }
