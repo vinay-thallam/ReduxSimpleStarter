d160ac4341453beefce3324a65298f655c0de717
vinay thallam <vinay.thallam@jifflenow.com>
2018-09-15 19:19:05 +0530

    added redux dev tool extension

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(
+  	reducers,
+  	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <App />
   </Provider>
   , document.querySelector('.container'));
